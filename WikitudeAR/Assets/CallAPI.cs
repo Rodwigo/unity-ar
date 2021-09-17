9adb1c8ababf52a35bd8f0bcaa7ce0e83d2030c6
Rodrigo Barbosa Caitano da Silva <68601982+Rodwigo@users.noreply.github.com>
2021-09-17 09:57:44 -0300

    added heroku api

---
+++ WikitudeAR/Assets/CallAPI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Net;
+using System.IO;
+
+public class CallAPI : MonoBehaviour
+{
+    public TextMeshProUGUI user;
+    string url = "https://api-totalpower.herokuapp.com/";
+    public string usuario;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadAPI());
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api-totalpower.herokuapp.com/");
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        string json = reader.ReadToEnd();
+
+        print("JSON response: " + json);
+
+        string[] splitArray = json.Split(char.Parse(":"));
+        user.text = splitArray[1];
+    }
+
+    IEnumerator LoadAPI()
+    {
+        yield return new WaitForSeconds(1);
+    }
+}
